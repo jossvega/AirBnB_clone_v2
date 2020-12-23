38c1cc8751144677fc11fd1f524e7c5950a14fd2
jossvega <jossvega103@gmail.com>
2020-12-22 22:51:05 -0500

    Fix conditional (mkdir -p versions)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-# generates a .tgz archive from the contents of the web_static folder
+"""
+Fabric script that generates a .tgz archive from the contents
+of the web_static folder
+"""
+
 from fabric.api import local
 from datetime import datetime
-import os.path
 
 
 def do_pack():
-    """ Generates a .tgz tar gzipped archive from the contents
-        of the web_static folder of your AirBnB Clone repo
-    """
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month,
-                                                         dt.day, dt.hour,
-                                                         dt.minute, dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
+    """ function that packs archives """
+    local('mkdir -p versions')
+    date = datetime.now()
+    file = local('tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz web_static'
+                   .format(date.year, date.month, date.day,
+                           date.hour, date.minute, date.second), capture=True)
+    if file.succeeded:
+        return file
+    return None
