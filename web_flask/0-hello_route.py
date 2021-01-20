dd1d5387d4da3a10392f5cd0b6dec8673a1073bb
jossvega <jossvega103@gmail.com>
2021-01-20 11:28:47 -0500

    Fix pycodstryle | Add README

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,6 +3,8 @@
 Your web application must be listening on 0.0.0.0, port 5000
 In Route /: display “Hello HBNB!”
 You must use the option strict_slashes=False in your route definition"""
+
+
 from flask import Flask
 app = Flask(__name__)
 
